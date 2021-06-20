ead7cb5fff80bca5137b07e210ad34523ba825c2
David Poza Suárez <pozasuarez@gmail.com>
2021-06-20 16:23:52 +0200

    current data using openweathermap

--- app.js
+++ app.js
@@ -11,11 +11,10 @@ const FTP_PASSWORD = process.env.FTP_PASSWORD;
 const FTP_BASE_PATH = process.env.FTP_BASE_PATH; // without trailing backslash
 const LOCAL_PATH = process.env.LOCAL_PATH; // without trailing backslash
 const OPENWEATHERMAP_API_KEY = process.env.OPENWEATHERMAP_API_KEY;
-const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+
 
 const TOMORROWIO_ENDPOINT = '';
 const AEMET_ENDPOINT = '';
-const WEATHERAPI_ENDPOINT = `http://api.weatherapi.com/v1`;
 const OPENWEATHERMAP_ENDPOINT = `https://api.openweathermap.org/data/2.5/onecall?appid=${OPENWEATHERMAP_API_KEY}&units=metric`;
 
 const locations = [
@@ -24,64 +23,72 @@ const locations = [
     lon: '-3.7750177',
     lat: '40.666554',
     pollen: true,
+    air_quality: true,
+    text_forecast: true,
   }
 ];
 
 function transformCurrent(data) {
-  console.log(">>", data)
   return ({
-    visibility: data.vis_km,
-    temperature: data.temp_c,
-    condition: data.condition.code,
-    feels_like: data.feelslike_c,
-    precipitation: data.precip_mm,
-    pressure: data.pressure_mb,
-    wind_speed: data.wind_kph,
-    wind_gust_speed: data.gust_kph,
-    wind_direction: data.wind_degree,
+    visibility: data.visibility,
+    temperature: data.temp,
+    weather: data.weather[0].icon,
+    feels_like: data.feels_like,
     humidity: data.humidity,
-    cloud_cover: data.cloud,
-    uv: data.uv,
-    air_quality: {
-      co: data['air_quality'].co,
-      no2: data['air_quality'].no2,
-      o3: data['air_quality'].o3,
-      so2: data['air_quality'].so2,
-      pm2_5: data['air_quality'].pm2_5,
-      pm10: data['air_quality'].pm10,
-      'us-epa-index': data['air_quality']['us-epa-index'],
-      'gb-defra-index': data['air_quality']['gb-defra-index']
-    },
+    wind_speed: data.wind_speed,
+    wind_direction: data.wind_deg,
+    humidity: data.humidity,
+    cloud_cover: data.clouds,
+    uvi: data.uvi,
+    sunrise: data.sunrise,
+    sunset: data.sunset,
   });
 }
 
+function transformForecastDay(data) {
+  return ({
+    date: data.dt,
+    max_temp: data.temp.max,
+    min_temp: data.temp.min,
+    weather: data.weather[0].icon,
+    wind_speed: data.wind_speed,
+    probability_of_precipitation: data.pop,
+    rain: data.rain,
+    moonrise: data.moonrise,
+    moonset:  data.moonset,
+    moon_phase: data.moon_phase,
+  });
+}
+
+function transformForecastHour(data) {
+  return ({
+    date: data.dt,
+    temp: data.temp,
+    weather: data.weather[0].icon,
+    wind_speed: data.wind_speed,
+    probability_of_precipitation: data.pop,
+    rain: data.rain?.['1h'],
+  });
+}
 
 async function fetchWeatherApi(lon, lat) {
-  let currentData;
-  let forecastData;
+  let data;
   try {
-    const res = await fetch(`${WEATHERAPI_ENDPOINT}/current.json?key=${WEATHER_API_KEY}&q=${lat},${lon}&aqi=yes`);
-    currentData = await res.json();
-    currentData = transformCurrent(currentData.current);
+    const res = await fetch(`${OPENWEATHERMAP_ENDPOINT}&lon=${lon}&lat=${lat}`);
+    data = await res.json();
   } catch (Error) {
     console.log('Error during weatherapi current fetch');
   }
 
-  try {
-    const res = await fetch(`${WEATHERAPI_ENDPOINT}/forecast.json?key=${WEATHER_API_KEY}&days=7&q=${lat},${lon}&aqi=yes`);
-    forecastData = await res.json();
-  } catch (Error) {
-    console.log('Error during weatherapi forecast fetch');
-  }
   return ({
-    current: currentData,
-    forecast: forecastData,
+    current: transformCurrent(data.current),
+    daily_forecast: data.daily.map(f => transformForecastDay(f)),
+    hourly_forecast: data.hourly.map(f => transformForecastHour(f)),
   });
 }
 
 async function buildFinalJson(lon, lat) {
   const weatherapi = await fetchWeatherApi(lon, lat);
-  console.log(weatherapi);
   return({
     ts: new Date().getTime(),
     data: {
@@ -94,7 +101,7 @@ async function buildFinalJson(lon, lat) {
 
 async function runJob() {
   for (loc of locations) {
-    console.log(await buildFinalJson(loc.lon, loc.lat));
+    console.log(await (await buildFinalJson(loc.lon, loc.lat)).data.hourly_forecast);
   };
   process.exit();
 }
